f092ac7f6788ad720f3183dbe44d689c67e017ef
Łukasz <lukaszsowa@onet.pl>
2020-07-13 22:12:18 +0200

    Fix problem with display user's details and style

--- src/main/java/pl/lukaszsowa/CRM/controller/UserController.java
+++ src/main/java/pl/lukaszsowa/CRM/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import pl.lukaszsowa.CRM.model.Role;
 import pl.lukaszsowa.CRM.model.User;
@@ -13,6 +14,7 @@ import pl.lukaszsowa.CRM.service.RoleService;
 import pl.lukaszsowa.CRM.service.UserService;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -24,9 +26,7 @@ public class UserController {
     @Autowired
     RoleService roleService;
 
-
-    @GetMapping("/users")
-    public String getUsersPage(Model model){
+    public void getLoggedUserInfo(Model model) {
         model.addAttribute("usersList", userService.getUsers());
         Authentication loggedUser = SecurityContextHolder.getContext().getAuthentication();
         String login = loggedUser.getName();
@@ -34,31 +34,46 @@ public class UserController {
         String role = userService.getUser(login).getRole().getRole().toUpperCase();
         model.addAttribute("fullName", fullName);
         model.addAttribute("role", role);
+    }
+
+
+    @GetMapping("/users")
+    public String getUsersPage(Model model){
+        getLoggedUserInfo(model);
         return "users";
     }
 
     @GetMapping("users/add")
     public String addUser(Model model){
         model.addAttribute("user", new User());
-        Authentication loggedUser = SecurityContextHolder.getContext().getAuthentication();
-        String login = loggedUser.getName();
-        String fullName = userService.getUser(login).getFirstName() + " " + userService.getUser(login).getLastName();
-        String role = userService.getUser(login).getRole().getRole().toUpperCase();
-        model.addAttribute("fullName", fullName);
-        model.addAttribute("role", role);
+        getLoggedUserInfo(model);
         return "users-add";
     }
 
     @PostMapping("/save-user")
     public String saveUser(@Valid @ModelAttribute User user, BindingResult bindingResult, Model model){
+        getLoggedUserInfo(model);
         if(bindingResult.hasErrors()){
-            return "home";
+            return "users-add";
         } else {
             model.addAttribute("user", new User());
             user.setRole(roleService.getRole("user"));
             userService.addUser(user);
-            return "home";
         }
+        return "redirect:/users";
+    }
+
+    @PostMapping("/save-changes")
+    public String saveChanges(@Valid @ModelAttribute User user, BindingResult bindingResult, Model model){
+        getLoggedUserInfo(model);
+        if (bindingResult.hasErrors()){
+            return "users-details-edit";
+        } else {
+            model.addAttribute("user", new User());
+            user.setRole(roleService.getRole("user"));
+            userService.addUser(user);
+        }
+        return "redirect:/users";
     }
 
     @RequestMapping(value = "/users/delete/{id}", method = RequestMethod.GET)
@@ -69,12 +84,7 @@ public class UserController {
 
     @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
     public String getUserDetails(@PathVariable("id") long id, Model model){
-        Authentication loggedUser = SecurityContextHolder.getContext().getAuthentication();
-        String login = loggedUser.getName();
-        String fullName = userService.getUser(login).getFirstName() + " " + userService.getUser(login).getLastName();
-        String role = userService.getUser(login).getRole().getRole().toUpperCase();
-        model.addAttribute("fullName", fullName);
-        model.addAttribute("role", role);
+        getLoggedUserInfo(model);
         Optional<User> optionalUser = userService.getUserById(id);
         User user = optionalUser.get();
         model.addAttribute("user", user);
