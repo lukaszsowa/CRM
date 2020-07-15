3cd21ab6c309fe6cfa20743ec87b786d56975a6c
Łukasz <lukaszsowa@onet.pl>
2020-07-15 22:40:00 +0200

    Add digital clock on website

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,16 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("clock").innerText = time;
+    document.getElementById("clock").textContent = time;
+
+    setTimeout(showTime, 1000);
+    }
