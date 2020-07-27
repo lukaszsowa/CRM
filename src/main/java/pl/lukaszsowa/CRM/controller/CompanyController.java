d3207e04c117a345a7875cfa0496b0732dfcb6d9
Łukasz <lukaszsowa@onet.pl>
2020-07-27 22:04:05 +0200

    Create companies-add.html

---
+++ src/main/java/pl/lukaszsowa/CRM/controller/CompanyController.java
@@ -0,0 +1,33 @@
+package pl.lukaszsowa.CRM.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.lukaszsowa.CRM.model.Contact;
+import pl.lukaszsowa.CRM.service.UserService;
+
+@Controller
+public class CompanyController {
+
+    @Autowired
+    UserService userService;
+
+    public void getLoggedUserInfo(Model model) {
+        Authentication loggedUser = SecurityContextHolder.getContext().getAuthentication();
+        String login = loggedUser.getName();
+        String fullName = userService.getUser(login).getFirstName() + " " + userService.getUser(login).getLastName();
+        String role = userService.getUser(login).getRole().getRole().toUpperCase();
+        model.addAttribute("fullName", fullName);
+        model.addAttribute("role", role);
+    }
+
+    @GetMapping("/companies")
+    public String getCompanies(Model model){
+        model.addAttribute("contact", new Contact());
+        getLoggedUserInfo(model);
+        return "companies-add";
+    }
+}
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -14,3 +14,9 @@ function showTime(){
 
     setTimeout(showTime, 1000);
     }
+
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
